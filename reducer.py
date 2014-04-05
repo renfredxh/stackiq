99ddec90e47687207b6f9ec548162cc5fcd81ff7
Renfred Harper <renfredxh@gmail.com>
2014-04-05 08:52:20 -0400

    Add reducer.py

---
+++ reducer.py
@@ -0,0 +1,33 @@
+import json
+import sys
+
+def accumulate_data(data1, data2):
+    for k in data1:
+        if isinstance(data1[k], dict):
+            data1[k] = accumulate_data(data1[k], data2[k])
+        else:
+            data1[k] += data2[k]
+    return data1
+
+current_lang = None
+current_count = 0
+word = None
+
+for line in sys.stdin:
+    line = line.strip()
+    lang, data = line.split('\t', 1)
+    # Convert serialized data string to json ('sort -k1,1')
+    data = json.loads(data)
+
+    # Only works if sorted
+    if current_lang == lang:
+        current_data = accumulate_data(current_data, data)
+    else:
+        if current_lang:
+            print("{}\t{}".format(current_lang, current_data))
+        current_data = data
+        current_lang = lang
+
+# do not forget to output the last word if needed!
+if current_lang == lang:
+    print("{}\t{}".format(current_lang, current_data))
