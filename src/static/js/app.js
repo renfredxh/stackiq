e193bc936b158349183bef2959ed562481576e1c
John Moss <johnmoss96@gmail.com>
2014-04-06 08:58:30 -0400

    tons, v1.0

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -389,7 +389,298 @@ var readingLevelsJ = {
     "percentage_answered": 0, 
     "score": 0, 
     "favorite_count": 0
+  },
+  "english": {
+    "lang" : "English", 
+    "count": 116014, 
+    "composite_community": 265,
+    "composite_intellegence": 137.285513534346, 
+    "reading_level": 9.041334695331413, 
+    "word_count": 97, 
+    "min_age": 13
   }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+var readingLevelsW = {
+    "Javascript": {
+    "lang" : "Javascript",
+    "count": 82916, 
+    "view_count": 4397, 
+    "scores_to_views": 0, 
+    "composite_community": 258.7984982926355, 
+    "linguistics_data": {
+      "word_count": 88, 
+      "reading_level": 7.441810932887826, 
+      "min_age": 12
+    }, 
+    "composite_intellegence": 115.98732871053132, 
+    "answer_count": 2, 
+    "percentage_answered": 0.0012015017073612132, 
+    "score": 4, 
+    "favorite_count": 1
+  }, 
+  "C": {
+    "lang" : "C",
+    "count": 30157, 
+    "view_count": 2934, 
+    "scores_to_views": 0, 
+    "composite_community": 246.69006794058078, 
+    "linguistics_data": {
+      "word_count": 91, 
+      "reading_level": 7.500599906163235, 
+      "min_age": 12
+    }, 
+    "composite_intellegence": 116.57521844328541, 
+    "answer_count": 3, 
+    "percentage_answered": 0.0033099320594276215, 
+    "score": 4, 
+    "favorite_count": 1
+  }, 
+  "Java": {
+    "lang" : "Java",
+    "count": 109563, 
+    "view_count": 4061, 
+    "scores_to_views": 0, 
+    "composite_community": 259.0904903662704, 
+    "linguistics_data": {
+      "word_count": 97, 
+      "reading_level": 8.152434768559555, 
+      "min_age": 12
+    }, 
+    "composite_intellegence": 123.09356706724861, 
+    "answer_count": 2, 
+    "percentage_answered": 0.0009095096337223825, 
+    "score": 4, 
+    "favorite_count": 1
+  }, 
+  "Scala": {
+    "lang" : "Scala",
+    "count": 3403, 
+    "view_count": 2001, 
+    "scores_to_views": 0, 
+    "composite_community": 360.6314345201463, 
+    "linguistics_data": {
+      "word_count": 91, 
+      "reading_level": 8.307484165919178, 
+      "min_age": 12
+    }, 
+    "composite_intellegence": 124.64406104084483, 
+    "answer_count": 2, 
+    "percentage_answered": 0.029368565479854815, 
+    "score": 7, 
+    "favorite_count": 2
+  }, 
+  "C#": {
+    "lang" : "C#",
+    "count": 167623, 
+    "view_count": 2882, 
+    "scores_to_views": 0, 
+    "composite_community": 189.40547672358662, 
+    "linguistics_data": {
+      "word_count": 99, 
+      "reading_level": 8.121859764992005, 
+      "min_age": 12
+    }, 
+    "composite_intellegence": 122.7878170315731, 
+    "answer_count": 2, 
+    "percentage_answered": 0.0005945232764126626, 
+    "score": 3, 
+    "favorite_count": 1
+  }, 
+  "Delphi": {
+    "lang" : "Delphi",
+    "count": 8642, 
+    "view_count": 1757, 
+    "scores_to_views": 0, 
+    "composite_community": 168.4647567321008, 
+    "linguistics_data": {
+      "word_count": 109, 
+      "reading_level": 8.077034150137806, 
+      "min_age": 12
+    }, 
+    "composite_intellegence": 122.33956088303111, 
+    "answer_count": 2, 
+    "percentage_answered": 0.011535243267903486, 
+    "score": 3, 
+    "favorite_count": 1
+  }, 
+  ".NET": {
+    "lang" : ".NET",
+    "count": 36991, 
+    "view_count": 2081, 
+    "scores_to_views": 0, 
+    "composite_community": 187.3074557952168, 
+    "linguistics_data": {
+      "word_count": 109, 
+      "reading_level": 8.59104650479119, 
+      "min_age": 13
+    }, 
+    "composite_intellegence": 132.78263162894376, 
+    "answer_count": 2, 
+    "percentage_answered": 0.002692544204780353, 
+    "score": 3, 
+    "favorite_count": 1
+  }, 
+  "C++": {
+    "lang" : "C++",
+    "count": 56696, 
+    "view_count": 2506, 
+    "scores_to_views": 0, 
+    "composite_community": 248.24003362709846, 
+    "linguistics_data": {
+      "word_count": 101, 
+      "reading_level": 7.993457036836209, 
+      "min_age": 12
+    }, 
+    "composite_intellegence": 121.50378975001514, 
+    "answer_count": 3, 
+    "percentage_answered": 0.0017599663729046056, 
+    "score": 4, 
+    "favorite_count": 1
+  }, 
+  "Perl": {
+    "lang" : "Perl",
+    "count": 9509, 
+    "view_count": 2183, 
+    "scores_to_views": 0, 
+    "composite_community": 179.50576580698907, 
+    "linguistics_data": {
+      "word_count": 96, 
+      "reading_level": 7.460720317220851, 
+      "min_age": 12
+    }, 
+    "composite_intellegence": 116.17642255386156, 
+    "answer_count": 2, 
+    "percentage_answered": 0.010494234193019657, 
+    "score": 3, 
+    "favorite_count": 0
+  }, 
+  "Lisp": {
+    "lang" : "Lisp",
+    "count": 1149, 
+    "view_count": 1775, 
+    "scores_to_views": 0, 
+    "composite_community": 353.11929016868817, 
+    "linguistics_data": {
+      "word_count": 93, 
+      "reading_level": 7.866123727222361, 
+      "min_age": 12
+    }, 
+    "composite_intellegence": 120.23045665387667, 
+    "answer_count": 3, 
+    "percentage_answered": 0.08688070983130598, 
+    "score": 8, 
+    "favorite_count": 2
+  }, 
+  "Objective-C": {
+    "lang" : "Objective-C",
+    "count": 35788, 
+    "view_count": 2701, 
+    "scores_to_views": 0, 
+    "composite_community": 187.21927468028298, 
+    "linguistics_data": {
+      "word_count": 90, 
+      "reading_level": 8.083094822652406, 
+      "min_age": 12
+    }, 
+    "composite_intellegence": 122.40016760817711, 
+    "answer_count": 2, 
+    "percentage_answered": 0.00278072531972337, 
+    "score": 3, 
+    "favorite_count": 1
+  }, 
+  "Haskell": {
+    "lang" : "Haskell",
+    "count": 2797, 
+    "view_count": 1415, 
+    "scores_to_views": 0, 
+    "composite_community": 394.2857553722388, 
+    "linguistics_data": {
+      "word_count": 105, 
+      "reading_level": 8.22697438009871, 
+      "min_age": 12
+    }, 
+    "composite_intellegence": 123.83896318264016, 
+    "answer_count": 2, 
+    "percentage_answered": 0.035714244627748026, 
+    "score": 8, 
+    "favorite_count": 2
+  }, 
+  "HTML/CSS": {
+    "lang" : "HTML/CSS",
+    "count": 39579, 
+    "view_count": 4749, 
+    "scores_to_views": 0, 
+    "composite_community": 257.48138720149655, 
+    "linguistics_data": {
+      "word_count": 87, 
+      "reading_level": 6.918629583319638, 
+      "min_age": 11
+    }, 
+    "composite_intellegence": 105.66916852710578, 
+    "answer_count": 2, 
+    "percentage_answered": 0.002518612798501571, 
+    "score": 4, 
+    "favorite_count": 1
+  }, 
+  "Python": {
+    "lang" : "Python",
+    "count": 56049, 
+    "view_count": 3346, 
+    "scores_to_views": 0, 
+    "composite_community": 298.21992134746324, 
+    "linguistics_data": {
+      "word_count": 96, 
+      "reading_level": 7.779458136628454, 
+      "min_age": 12
+    }, 
+    "composite_intellegence": 119.3638007479376, 
+    "answer_count": 2, 
+    "percentage_answered": 0.0017800786525465496, 
+    "score": 5, 
+    "favorite_count": 2
+  }, 
+  "PHP": {
+    "lang" : "PHP",
+    "count": 105432, 
+    "view_count": 2271, 
+    "scores_to_views": 0, 
+    "composite_community": 139.05450807069428, 
+    "linguistics_data": {
+      "word_count": 92, 
+      "reading_level": 7.47289978670425, 
+      "min_age": 12
+    }, 
+    "composite_intellegence": 116.29821724869555, 
+    "answer_count": 2, 
+    "percentage_answered": 0.0009454919293139971, 
+    "score": 2, 
+    "favorite_count": 0
+  }, 
+  "Ruby": {
+    "lang" : "Ruby",
+    "count": 21133, 
+    "view_count": 2450, 
+    "scores_to_views": 0, 
+    "composite_community": 235.27791630041975, 
+    "linguistics_data": {
+      "word_count": 87, 
+      "reading_level": 7.586603529654306, 
+      "min_age": 12
+    }, 
+    "composite_intellegence": 117.43525467819612, 
+    "answer_count": 2, 
+    "percentage_answered": 0.004722083699581801, 
+    "score": 4, 
+    "favorite_count": 1
+  }
+
 }
       
       var readingLevels = d3.values(readingLevelsJ);
@@ -474,12 +765,14 @@ var readingLevelsJ = {
       //var minRl = readingLevels[0].composite_intelligence, maxRl = readingLevels[0].composite_intelligence, minPop = readingLevels[0].count, maxPop = readingLevels[0].count, minAct = readingLevels[0].composite_community, maxAct = readingLevels[0].composite_community;
 
       for (x = 0; x < readingLevels.length; x++) {
-        minRl = Math.min(minRl, readingLevels[x].composite_intellegence);
-        maxRl = Math.max(maxRl, readingLevels[x].composite_intellegence);
-        minPop = Math.min(minPop, readingLevels[x].count);
-        maxPop = Math.max(maxPop, readingLevels[x].count);
-        minAct = Math.min(minAct, readingLevels[x].composite_community);
-        maxAct = Math.max(maxAct, readingLevels[x].composite_community);
+        if(readingLevels[x].lang != "English") {
+          minRl = Math.min(minRl, readingLevels[x].composite_intellegence);
+          maxRl = Math.max(maxRl, readingLevels[x].composite_intellegence);
+          minPop = Math.min(minPop, readingLevels[x].count);
+          maxPop = Math.max(maxPop, readingLevels[x].count);
+          minAct = Math.min(minAct, readingLevels[x].composite_community);
+          maxAct = Math.max(maxAct, readingLevels[x].composite_community);
+        }
       }
 
       var scaleX = d3.scale.linear().domain([minRl,maxRl]).range([70,width-70]);
@@ -529,7 +822,8 @@ var readingLevelsJ = {
           .append("circle")
           .attr("r", function(d) { /*console.log(d);*/ d.radius = scaleR(d.count); return scaleR(d.count); })
           .style("fill", function(d){ 
-            if (d.lang == "Cooking") {
+            if (d.lang == "Cooking" || d.lang == "English") {
+              d3.select(this).attr("class","ne");
               return "#3CB5DE";
             }
             else {
@@ -547,7 +841,7 @@ var readingLevelsJ = {
 
 
       svg.selectAll("circle")
-          .attr("class","n")
+          //.attr("class","n")
           .attr("cx", function(d) { return scaleX(d.composite_intellegence); })
           .attr("cy", function(d) { return scaleY(d.composite_community); })
 
@@ -635,17 +929,28 @@ var readingLevelsJ = {
       d3.select(".dropdown-menu").selectAll("li").data(languages).enter().append("li").append("a").attr("id", function(d) { return d; }).attr("tabindex", "-1").attr("href", "#").text(function(d) { return d; });
 
   svg.selectAll('circle').on('click', function(d) {
-    lang_name = d.lang;
-    $('#svg_main').slideToggle();
-    loadPage(lang_name);
-    $("#lang-info").slideToggle();
-    $("#help-btn").fadeOut();
-    $("#reset-btn").fadeOut();
-    $(".slider").fadeOut();
-    //console.log($("#lang-name"))
-    toggleLangInfoMode();
+    ///////////////////////////////////////////////////////////// NON INTERACTIVE TOPICS
+    if (d.lang != "Cooking" || d.lang != "English") {
+      lang_name = d.lang;
+      $('#svg_main').slideToggle();
+      loadPage(lang_name);
+      $("#lang-info").slideToggle();
+      $("#help-btn").fadeOut();
+      $("#reset-btn").fadeOut();
+      $(".slider").fadeOut();
+      //console.log($("#lang-name"))
+      toggleLangInfoMode();
+    }
   });
 
+  $("[name='eng-check']").bootstrapSwitch({
+    "size":"small",
+    "onSwitchChange":blueTog
+  })
+
+  function blueTog() {
+    $(".ne").parent().toggle("slow");
+  }
 
   //Set things up
   $(".slider").noUiSlider({
@@ -735,11 +1040,11 @@ function parseKey(k) {
   var rec = [];
   var total = 0;
 
-  $.each(readingLevels, function(index, value) {
-    //rec.lang = value.lang;
-    var lang = value[lang];
-    rec.push({"lang": value["lang"], "v": value[k]});
-    total += value[k];
+  $.each(readingLevelsW, function(index, value) {
+      //rec.lang = value.lang;
+      var lang = value[lang];
+      rec.push({"lang": value["lang"], "v": value[k]});
+      total += value[k];
   });
 
   rec.push({ "lang": "total", "v": total });
@@ -752,7 +1057,7 @@ function parseKeyL(k) {
   var rec = [];
   var total = 0;
 
-  $.each(readingLevels, function(index, value) {
+  $.each(readingLevelsW, function(index, value) {
     //rec.lang = value.lang;
     var lang = value[lang];
     rec.push({"lang": value["lang"], "v": value.linguistics_data[k]});
@@ -786,7 +1091,6 @@ $(".dg").click(function() {
   }
 
   arr.sortByProp("v");
-  console.log(arr);
 
   dmin = arr[0].v;
   dmax = arr[0].v;
@@ -795,6 +1099,7 @@ $(".dg").click(function() {
         dmin = Math.min(dmin, arr[x].v);
         dmax = Math.max(dmax, arr[x].v);
   }
+
 
   console.log(dmin)
   console.log(dmax)
